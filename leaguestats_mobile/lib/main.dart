f91a5b7021cbac274395579e995b117808e980a6
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2026-02-17 01:05:13 +0100

    No avanzo mucho

--- leaguestats_mobile/lib/main.dart
+++ leaguestats_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:leaguestats_mobile/features/home/ui/home_page_view.dart';
+import 'package:leaguestats_mobile/features/home/ui/prueba.dart';
 import 'package:leaguestats_mobile/features/login/ui/login_page_view.dart';
 
 void main() {
