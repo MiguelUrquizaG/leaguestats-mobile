ec9340ceeaa2a6478ab8d90e214eed15d3bdb63d
Alumno <urquiza.gamig24@triana.salesianos.edu>
2026-03-02 14:18:47 +0100

    <feat>[Movil]

--- leaguestats_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ leaguestats_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_darwin
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
